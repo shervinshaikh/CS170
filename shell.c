dd1dd5bdb2ab2c6217235b1d88cb03665abdfd14
Shervin Shaikh <shervinater@gmail.com>
2013-04-19 09:20:45 -0700

    fork, execvp, and wait working for purely just the command

--- shell.c
+++ shell.c
@@ -1,6 +1,8 @@
-/*Shervin Shaikh & Ramon Rovirosa
-CS170
-Project 1 */
+/*
+Shervin Shaikh & Ramon Rovirosa
+CS 170
+Project 1
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,7 +17,6 @@ int main()
 {
   char inputString[MAXLINE] = {0};
   pid_t pid;
-  
 
   while(1){    
     printf("ssih:>");
@@ -24,22 +25,34 @@ int main()
     // replaces newline with null
     inputString[strlen(inputString) - 1] = 0;
 
-    if(strcmp(inputString, "exit") == 0){
-      exit(0);
-    }
-    else if(strcmp(inputString, "pwd") == 0){
-      char directory[MAXLINE];
-      getcwd(directory, 1024);
-      printf("%s\n", directory);
-    }
+    // exits the shell
+    if((!strcmp(inputString, "exit")) || feof(stdin)) exit(0);
 
-    else if(strncmp(inputString, "echo ", 5) == 0){
-      //char restOfLine[MAXLINE-4] = {0};
-      char *restOfLine = inputString + 5;
-      printf("%s\n", restOfLine);
-    }
+    // else if(strcmp(inputString, "pwd") == 0){
+    //   char directory[MAXLINE];
+    //   getcwd(directory, 1024);
+    //   printf("%s\n", directory);
+    // }
+    // else if(strncmp(inputString, "echo ", 5) == 0){
+    //   //char restOfLine[MAXLINE-4] = {0};
+    //   char *restOfLine = inputString + 5;
+    //   printf("%s\n", restOfLine);
+    // }
     else{
-      printf("ERROR: \"%s\" is not a proper command\n", inputString);
+      int proper_cmd;
+      // char *args[]={"/bin/ls","-al", (char*)0 };
+      char *args[]={inputString, 0, (char*)0 };
+      pid = fork();
+      if(pid == 0){
+        proper_cmd = execvp(inputString, args);
+        if(proper_cmd<0){
+          printf("ERROR: %s: command not found\n", inputString);
+          exit(1);
+        }
+      }
+      else{
+        wait(NULL);
+      }
     }
 
     
@@ -52,7 +65,7 @@ int main()
     //   // in the child process
     //   //char cmd[20]={"/bin/ls"};
     //   char cmd[20]={"ls"};
-    //   // char *args[]={"/bin/ls","-al", (char*)0 };
+    //   
     //   char *args[]={"/","-l", (char*)0 };
     //   execvp(inputString, args);
     // }
