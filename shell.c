71e6e30323589a0805666ff3d43a2887b712be80
ramon.rovirosa@gmail.com <ramon.rovirosa@gmail.com>
2013-04-18 20:20:43 -0700

    Add stdin functionality

--- shell.c
+++ shell.c
@@ -7,7 +7,12 @@ Project 1 */
 int main()
 {
   while(1){
+
+    char inputString[1024] = {0};
+    int MAX = 1024;
+
     printf("ssih:>");
+    fgets(inputString, MAX, stdin);
     //added comment
 
   }
