16e6dc67336325b2c10b866d7822db84b195e349
ramon.rovirosa@gmail.com <ramon.rovirosa@gmail.com>
2013-04-23 00:58:43 -0700

    parsing wors: method parseString2() char inpustring -> char**command

--- parser.c
+++ parser.c
@@ -4,15 +4,9 @@
 
 char*** parseString(char * inputstring);
 
-void main()
-{
-  char theString[] = "hello world      |     cat f > dog world | hat me";
-  char ***FinalStrings = parseString(theString);
-  //printf("FinalStrings:  %s \n", **FinalStrings);
-}
-
-//**char[] parseString(char* inputString){
-char ***parseString(char* inputString){
+void parseString2(){
+  char inputString[1024] = "hello world      |     cat f > dog world | hat me";
+  
   //find how many redirect commands there are
   int slen = strlen(inputString), nredirects = 0, npipes = 0;
   int i;
@@ -26,7 +20,7 @@ char ***parseString(char* inputString){
      }
    }
 
-   // place each command into a string array
+  // place each command into a string array
    char *cmd[nredirects+1];
    i = 1;
    cmd[0] = strtok(inputString, "<|>");
@@ -38,40 +32,41 @@ char ***parseString(char* inputString){
      i++;
    }
 
-   char **parsedArray[nredirects+1];
-   char ***parsedArray2 = (char ***)malloc(1000*sizeof(char *));
-
    int j;
-   char *args[1024];
+   char *args[nredirects+1][1024];
    for(j=0;j<=nredirects;j++){
      int count = 1;
-     args[0] = strtok(cmd[j], " ");
+     args[j][0] = strtok(cmd[j], " ");
      //printf("args[0]: %s\n", args[0]);
-     while(args[count-1] != NULL){
-       args[count] = strtok(NULL, " ");
+     while(args[j][count-1] != NULL){
+       args[j][count] = strtok(NULL, " ");
        //printf("args[%d]: %s\n",count, args[count]);
        count++;
      }
-     parsedArray[j] = args;
-     printf("first %s \n", **parsedArray);
-
+     //parsedArray[j] = args;
+     // printf("first %s \n", args[j]);
    }
 
-   //printf("%s\n",**parsedArray);
-   // printf("%s\n",**parsedArray[1]);
-   //printf("%s\n",**parsedArray[2]);
-   //printf("%s\n",**parsedArray[3]);
+   //printf("first %s \n", args[2][0]);
+
+   char **command[nredirects+1];
+   //commands = {args[0],args[1],args[2],args[3]};
+   int k;
+   for(k=0;k<=nredirects;k++){
+     command[k] = args[k]; 
+   }
+   printf("command[k]: %s \n", command[1][0]);
 
 
-   //int k;
-   //for(k=0;k<=nredirects;k++){
-   //  parsedArray2[k] = parsedArray[k];
-   //  printf("ParsedArray2 %s \n", **parsedArray2);
-   //}
-
-
-   parsedArray2 = parsedArray;
-   //printf("parsedArray2: %s\n", parsedArray2[10][0]);
-   return parsedArray2;
    
 }
+
+void main()
+{
+  char theString[] = "hello world      |     cat f > dog world | hat me";
+  //char ***FinalStrings = parseString(theString);
+  //printf("FinalStrings:  %s \n", **FinalStrings);
+  parseString2();
+}
+
+
